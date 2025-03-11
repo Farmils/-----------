8d03cf28bf9a76598342e96eceb11d775cd0d050
Клим <jjj>
2025-03-11 16:26:33 +0300

    sendFine

--- Deploy.bat
+++ Deploy.bat
@@ -1,3 +1,3 @@
 CD .\fabric-samples\test-network
-WSL ./network.sh deployCC -ccn sendFine -ccl javascript -c blockchain2025 -ccp ../my-project/chaincode -cci InitLedger
+WSL ./network.sh deployCC -ccn jjopa -ccl javascript -c blockchain2025 -ccp ../my-project/chaincode -cci InitLedger
 PAUSE
